a44dd7bf68e413ea2801a466e7d83542eee9049d
Mark Notton <mark@yellostudio.co.uk>
2019-01-18 14:01:13 +0000

    Fixed - The touch device checker as per these instructions: https://stackoverflow.com/a/4819886/843131

--- src/assets/scripts/agent.js
+++ src/assets/scripts/agent.js
@@ -111,7 +111,17 @@ class Agent {
   }
 
   get touch () {
-    return 'ontouchstart' in window || window.navigator.msPointerEnabled || 'ontouchstart' in document.documentElement;
+	  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+	  var mq = function(query) {
+	    return window.matchMedia(query).matches;
+	  }
+
+	  if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+	    return true;
+	  }
+
+	  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+	  return mq(query);
   }
 
   get viewport () {
